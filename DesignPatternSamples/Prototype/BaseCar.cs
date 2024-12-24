c3751b5ca34eb72b97f4d4149a039dc33c1caefd
Javid <javidleo.ef@gmail.com>
2024-12-24 21:44:57 +0330

    adding prototype pattern

---
+++ DesignPatternSamples/Prototype/BaseCar.cs
@@ -0,0 +1,50 @@
+﻿namespace Prototype;
+
+public class Program
+{
+    public static void Main()
+    {
+        BaseCar baseFord = new Ford("b24");
+        // creating a copy instead of creating a new one.
+        BaseCar copyFord = baseFord.Clone();
+
+        // then change the copy as you want.
+        copyFord.Price += 1000;
+    }
+}
+public abstract class BaseCar
+{
+    protected BaseCar(string name)
+    {
+        Name = name;
+    }
+    public int Price { get; set; }
+    public string Name { get; set; }
+
+    public abstract BaseCar Clone();
+}
+
+public class Ford : BaseCar
+{
+    public Ford(string name) : base(name)
+    {
+    }
+
+    public override BaseCar Clone()
+    {
+        return (Ford)MemberwiseClone();
+    }
+}
+
+public class Ferrari : BaseCar
+{
+    public Ferrari(string name) : base(name)
+    {
+
+    }
+
+    public override BaseCar Clone()
+    {
+        return (Ferrari)MemberwiseClone();
+    }
+}
---
+++ DesignPatternSamples/Prototype/README.md
@@ -0,0 +1,12 @@
+this is a basic sample of prototype pattern with explanation
+
+## what we found out of it? 
+well, it's worthy to use when the creation of an object is expensive, or you create objects of same type with really similar data
+
+basically, you don't need to create the object again if you need the same thing with some changes, 
+
+you can copy it and make the changes on the copy, it depends on your scenario but 
+
+c# introduced the 'Record' which solves the same problem. 
+
+I didn't get anything else more from this pattern. 
